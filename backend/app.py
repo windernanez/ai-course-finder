7b71990ae3c1cfe80fecfa687d8ac64efe31aae3
VIT-PC <doriannyroblesgauna@gmail.com>
2026-04-06 11:17:13 -0400

    fix: usar rutas absolutas para evitar errores 404 en producción

--- backend/app.py
+++ backend/app.py
@@ -29,8 +29,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Cargar variables de entorno
 load_dotenv()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+frontend_dir = os.path.abspath(os.path.join(basedir, '..', 'frontend'))
+
 app = Flask(__name__, 
-            static_folder='../frontend',
+            static_folder=frontend_dir,
             static_url_path='')
 CORS(app)
 
